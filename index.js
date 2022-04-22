7b9d5b801461509c507fd110ad5d2dda6f20ec58
archaic10 <vitor.franca.9400@gmail.com>
2022-04-22 17:11:28 -0300

    feat!: change url-jira to domain.

--- index.js
+++ index.js
@@ -1,52 +1,73 @@
 
 const core = require('@actions/core')
 const axios = require('axios')
+const github = require('@actions/github')
+
 var dateDeploy = null
 async function run (){
     try{
-        const url = core.getInput('url-jira')
+        const domain = core.getInput('domain')
         const basic_auth = core.getInput('basic-auth')
-        if(url && basic_auth){        
-            await getDateDeploy(url, basic_auth)            
-            await validationDateDeploy()
+        if(domain && basic_auth){        
+            let serviceDesk = await getDataIssue(domain, basic_auth)
+            if(serviceDesk != null){
+                await getDateDeploy(domain, serviceDesk,basic_auth)       
+                await validationDateDeploy()
+            }else{
+                core.setFailed("Não foi estabelecida data para deploy para essa GMUD!")
+                return
+            }
         }else{
-            core.setFailed("invalid url or basic_auth")
+            core.setFailed("O parâmetro domain ou basic_auth é inválido!")
         }
     }catch(error){
         core.setFailed("Parâmetros inválidos")
     }
-    
 }
 
-async function getDateDeploy(url, basic_auth){
+async function getDataIssue(domain,  basic_auth){
     try{
-        await axios.get(url, {
+            let titlePR = github.context.payload.pull_request.title
+            let keyJira = titlePR.split("(").pop().split(")")[0]
+            return  axios.get( `https://${domain}.atlassian.net/rest/api/3/issue/${keyJira}`, {
+                headers: {
+                    Authorization: basic_auth,
+                }
+            }).then((response)=>{
+                if(!Object.keys(response.data.fields.issuelinks).length == 0){
+                    dateDeploy = response.data.fields.customfield_10476
+                    return response.data.fields.issuelinks[0].outwardIssue.key       
+                }
+                return null
+            }).catch((error)=>{        
+                core.setFailed("Erro ao buscar data de deploy", error)
+            })
+    }catch(error){
+        core.setFailed("Essa action só funcionará em uma pull request!")
+    }
+}
+
+async function getDateDeploy(domain, serviceDesk ,basic_auth){
+    try{
+        await axios.get(`https://${domain}.atlassian.net/rest/api/3/issue/${serviceDesk}`, {
             headers: {
                 Authorization: basic_auth,
             }
         }).then(function(response){            
-            dateDeploy = response.data.fields.customfield_10476            
+            dateDeploy = response.data.fields.customfield_10476
         })
     }catch(error){        
         core.setFailed("Erro ao buscar data de deploy")
     }
 }
 
-
-
- async function validationDateDeploy(){
+async function validationDateDeploy(){
     try{
         let dateNow = new Date()
-        dateDeploy = new Date(dateDeploy).toLocaleDateString('pt-BR', {
-            hour: '2-digit'
-        })
-        dateNow = new Date(dateNow).toLocaleDateString('pt-BR', {
-            hour: '2-digit'
-        })
-
-        if (dateNow == dateDeploy) {       
+        dateDeploy = new Date(dateDeploy)
+        if(dateNow <= dateDeploy){
             core.setOutput("result", "Data do deploy é igual a data atual")    
-        } else {
+        }else {
             core.setFailed("Data de deploy não está correta")
         }
     }catch(error){
